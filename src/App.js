2db80fd43afdf1438b8b37696331ad915d7d2f58
Sofiane M <poubellepropre8@gmail.com>
2022-05-28 10:06:15 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import contact from './contacts.json';
+import { useState } from 'react';
 
 function App() {
+  const [contacts, setContacts] = useState(contact.slice(0, 5));
+
+  const updatedContact = () => {
+    const newA = Math.floor(Math.random() * contact.length);
+    const updatedContact = [...contacts, contact[newA]];
+
+    setContacts(updatedContact);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <button>Add Random Contact</button>
+      <table>
+        <th>Pictures</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </table>
+      {contacts.map((contact) => {
+        return (
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <img src={contact.pictureUrl} alt={contact.name} width="50" />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+                <td>{contact.wonOscar ? 'yes' : 'no'}</td>
+                <td>{contact.wonEmmy ? 'yes' : 'no'}</td>
+              </tr>
+            </tbody>
+          </table>
+        );
+      })}
     </div>
   );
 }
